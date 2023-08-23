38377a28fc8cbf0ba89ad9c7ca8df0325bab6340
crazymma0 <crazypriz8082@gmailcom>
2023-08-23 23:03:32 +0600

    Initial commit

---
+++ database/sql_queries.py
@@ -0,0 +1,53 @@
+CREATE_USER_TABLE_QUERY = """
+        CREATE TABLE IF NOT EXISTS telegram_users
+        (ID INTEGER PRIMARY KEY,
+        TELEGRAM_ID INTEGER,
+        USERNAME CHAR(50),        
+        FIRST_NAME CHAR(50),        
+        LAST_NAME CHAR(50),
+        UNIQUE (TELEGRAM_ID)
+        )
+"""
+CREATE_USER_FORM_QUERY = """
+        CREATE TABLE IF NOT EXISTS user_form
+        (ID INTEGER PRIMARY KEY,
+        TELEGRAM_ID INTEGER,
+        NICKNAME CHAR(50),
+        AGE INTEGER,
+        BIO TEXT,
+        PHOTO TEXT,
+        UNIQUE (TELEGRAM_ID)
+        )
+
+"""
+
+CREATE_FORM_LIKE_QUERY = """
+        CREATE TABLE IF NOT EXISTS form_like
+        (ID INTEGER PRIMARY KEY,
+        OWNER_TELEGRAM_ID INTEGER,
+        LIKER_TELEGRAM_ID INTEGER
+        )
+
+"""
+START_INSERT_USER_QUERY = """INSERT OR IGNORE INTO telegram_users VALUES (?,?,?,?,?)"""
+
+SELECT_USER_QUERY = """SELECT telegram_id, username, first_name FROM telegram_users"""
+
+INSERT_USER_FORM_QUERY = """INSERT OR IGNORE INTO user_form VALUES (?,?,?,?,?,?)"""
+
+SELECT_USER_FORM_BY_TELEGRAM_ID_QUERY = """SELECT * FROM user_form WHERE TELEGRAM_ID = ?"""
+
+SELECT_USER_FORM_QUERY = """SELECT * FROM user_form"""
+
+INSERT_LIKE_FORM_QUERY = """INSERT OR IGNORE INTO form_like VALUES (?,?,?)"""
+
+SELECT_LIKED_FORM_QUERY = """
+SELECT * FROM form_like WHERE OWNER_TELEGRAM_ID = ? AND LIKER_TELEGRAM_ID = ?"""
+
+UPDATE_USER_FORM_QUERY = """
+UPDATE user_form SET NICKNAME = ?, AGE = ?, BIO = ?, PHOTO = ? WHERE TELEGRAM_ID = ?
+"""
+
+DELETE_USER_FORM_QUERY = """
+DELETE FROM user_form WHERE TELEGRAM_ID = ?
+"""
---
+++ keyboards/start_keyboard.py
@@ -0,0 +1,109 @@
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+
+
+# async def start_kb():
+#     quiz_button = KeyboardButton("/quiz")
+#     mark_up = ReplyKeyboardMarkup(one_time_keyboard=True,
+#                                   resize_keyboard=True)
+#
+#     mark_up.add(quiz_button)
+#     return mark_up
+
+
+async def quiz_1_keyboard():
+    markup = InlineKeyboardMarkup()
+    button_call_1 = InlineKeyboardButton(
+        "Следующая Викторина",
+        callback_data="button_call_1"
+    )
+    markup.row(
+        button_call_1
+    )
+    return markup
+
+
+async def quiz_2_keyboard():
+    markup = InlineKeyboardMarkup()
+    button_call_1 = InlineKeyboardButton(
+        "Male",
+        callback_data="answer_male"
+    )
+    button_call_2 = InlineKeyboardButton(
+        "Female",
+        callback_data="answer_female"
+    )
+    markup.row(
+        button_call_1,
+        button_call_2
+    )
+    return markup
+
+
+async def admin_select_user_keyboard():
+    markup = InlineKeyboardMarkup()
+    admin_user_list = InlineKeyboardButton(
+        "Список пользователей",
+        callback_data="admin_user_list"
+    )
+
+    markup.row(
+        admin_user_list
+    )
+    return markup
+
+
+async def new_start_kb():
+    markup = InlineKeyboardMarkup()
+    random_profiles_button = InlineKeyboardButton(
+        "Анкеты для просмотра",
+        callback_data="random_profiles"
+    )
+    my_profile_button = InlineKeyboardButton(
+        "Мой Профиль",
+        callback_data="my_profile"
+    )
+
+    markup.row(
+        random_profiles_button,
+        my_profile_button
+    )
+    return markup
+
+
+async def like_dislike_profiles_keyboard(form_telegram_id):
+    markup = InlineKeyboardMarkup(row_width=2)
+    like_button = InlineKeyboardButton(
+        "👍🏻",
+        callback_data=f"like_{form_telegram_id}"
+    )
+    dislike_button = InlineKeyboardButton(
+        "👎🏻",
+        callback_data="random_profiles"
+    )
+    markup.row(
+        like_button,
+        dislike_button
+    )
+    return markup
+
+
+async def my_profile_detail_keyboard(telegram_id):
+    markup = InlineKeyboardMarkup(row_width=2)
+    update_button = InlineKeyboardButton(
+        "Изменить Анкету 💡",
+        callback_data="signup"
+    )
+    delete_button = InlineKeyboardButton(
+        "Удалить Анкету ❌",
+        callback_data=f"delete_form_{telegram_id}"
+    )
+    markup.row(
+        update_button,
+        delete_button
+    )
+    return markup
